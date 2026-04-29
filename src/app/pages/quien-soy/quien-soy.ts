87db8d6ce510f4af007ddf53207310307c02271c
Juan Pablo Leiva <juampi.leiva5@gmail,com>
2026-04-29 10:04:07 -0300

    implementacion de estilo a quien soy

--- src/app/pages/quien-soy/quien-soy.ts
+++ src/app/pages/quien-soy/quien-soy.ts
@@ -1,23 +1,20 @@
-import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-quien-soy',
-  standalone: true,
   imports: [CommonModule],
   templateUrl: './quien-soy.html',
-  styleUrls: ['./quien-soy.css']
+  styleUrl: './quien-soy.css'
 })
 export class QuienSoy implements OnInit {
-
   usuario: any;
 
   ngOnInit() {
     fetch('https://api.github.com/users/juampileiva')
-      .then(res => res.json())
-      .then(data => {
-        this.usuario = data;
+      .then((respuesta) => respuesta.json())
+      .then((datos) => {
+        this.usuario = datos;
       });
   }
 }
