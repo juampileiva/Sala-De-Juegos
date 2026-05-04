ff93bf92dd7e87667604c780a7b38b8ad816c546
Juan Pablo Leiva <juampi.leiva5@gmail,com>
2026-05-03 22:05:56 -0300

    Actualizacion del  home segun estado de sesion

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+
+import { supabase } from '../../services/supabase';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink],
+  imports: [CommonModule, RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
-export class Home {
+export class Home implements OnInit {
+  emailUsuario = '';
+  estaLogueado = false;
 
+  async ngOnInit() {
+    const { data } = await supabase.auth.getUser();
+
+    if (data.user) {
+      this.estaLogueado = true;
+      this.emailUsuario = data.user.email || '';
+    }
+  }
+
+  async cerrarSesion() {
+    await supabase.auth.signOut();
+
+    this.estaLogueado = false;
+    this.emailUsuario = '';
+  }
 }
